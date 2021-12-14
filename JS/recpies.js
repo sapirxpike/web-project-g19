e6fac82f34e2efba1920123a5fc629486bb75bf5
Alaa Nassar <alaa242.na@gmail.com>
2021-12-14 11:17:25 +0200

    js for recpices half done

--- JS/recpies.js
+++ JS/recpies.js
@@ -1,4 +1,6 @@
-let recpies = { israel: { name: "Hummus", ingredients: ["Chickpeas","Garlic","Kosher Salt","Garnish","Tahini"]  }, mexico: { name: "Burrito" }, japan: { name: "Sushi" } };
+let recpies = { israel: { name: "Hummus", ingredients: ["Chickpeas","Garlic","Kosher Salt","Garnish","Tahini"], srcImg: "imgs-project/hummus.jpeg"  }, 
+usa: { name: "Burger", ingredients: ["1 pound ground lean beef","1 large egg","half cup minced onion","1 tablespoon Worcestershire","1 or 2 cloves garlic, peeled and minced","tomato", "red onion"], srcImg: "imgs-project/american_burger.jpeg" }, 
+japan: { name: "Sushi",ingredients: ["sushi rice","a bamboo mat","low-sodium soy sauce","toasted sesame seeds","wasabi","pickled ginger", "nori"," chia seeds"], srcImg: "imgs-project/sushi_japan.jpeg" } };
 
 
 function getIngredients(recpie){
@@ -21,9 +23,29 @@ function getRecpies() {
     titleElm.innerHTML = recpie.name;
     getIngredients(recpie)
    
-    console.log(recpie)
+    
 }
 
-
-
 getRecpies()
+
+function getCountry(){
+    let url = new URL(window.location.href);
+    let country = url.searchParams.get("country");
+    let country2 = country.charAt(0).toUpperCase() + country.slice(1);
+    let countryTitle = document.getElementById("fw-country-name");
+    countryTitle.innerHTML = country2;
+}
+
+getCountry()
+
+
+function getRecpieImg(){
+    let url = new URL(window.location.href);
+    let country = url.searchParams.get("country");
+    let source = recpies[country].srcImg;
+    console.log(source);
+    let srcChange = document.getElementById("fw-recpie-img-in");
+    srcChange.src = source;
+}
+
+getRecpieImg()
